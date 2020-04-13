70489973b6e1efe243b3cd154cc02b59572e7fb5
peterkrumb <petekrumb@gmail.com>
2020-04-12 20:05:48 -0500

    add dropdown

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $("#select-player").on("click", function (event) {
   event.preventDefault();
 
-  var playerName = $("#player-input").val();
+  var playerName = $(".input").val();
   var settings = {
     url:
       "https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/players?search=" +
@@ -83,38 +83,88 @@ $("#select-player").on("click", function (event) {
   });
 });
 
-const input = document.querySelector("#player-input");
-
-let timeoutId;
-const onInput = (event) => {
-  var playerName = $("#player-input").val();
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  } else if (playerName.length >= 3) {
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
     timeoutId = setTimeout(() => {
-      event.preventDefault();
-
-      console.log(playerName.length);
-
-      var settings = {
-        url:
-          "https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/players?search=" +
-          playerName,
-        method: "GET",
-        timeout: 0,
-      };
-
-      //first ajax call
-      $.ajax(settings).done(function (response) {
-        //console.log(response.data[0]);
-        var playerID = response.data[0].id;
-        var playerFull =
-          response.data[0].first_name + " " + response.data[0].last_name;
-        console.log(playerFull);
-        console.log(playerID);
-      });
-    }, 1000);
-  }
+      func.apply(null, args);
+    }, delay);
+  };
 };
 
-input.addEventListener("input", onInput);
+const fetchData = async (searchTerm) => {
+  var playerName = $(".input").val();
+  const response = await axios.get(
+    "https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/players?per_page=100&search=" +
+      playerName,
+    {
+      params: {
+        playerName: searchTerm,
+      },
+    }
+  );
+
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.data;
+};
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<input class="input" placeholder="SEARCH FOR a player e.g. LeBron James" type="text"/>
+<div class="dropdown">
+  <div class="dropdown-menu">
+    <div class="dropdown-content results">
+    </div>
+  </div>
+</div>
+`;
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+const dropdownItem = document.querySelector(".dropdown-item");
+
+const onInput = async (event) => {
+  var playerName = $(".input").val();
+  event.preventDefault();
+  const players = await fetchData(event.target.value);
+  console.log(players.length);
+
+  // if (playerName.length < 3) {
+  //   dropdown.classList.remove("is-active");
+  //   return;
+  // }
+
+  resultsWrapper.innerHTML = "";
+  if (playerName.length >= 3 && players.length !== 0) {
+    dropdown.classList.add("is-active");
+    for (let player of players) {
+      console.log(player);
+      const option = document.createElement("a");
+      if (player.id <= 493 || player.id >= 666604) {
+        option.classList.add("dropdown-item");
+        option.innerHTML = `${player.first_name} ${player.last_name} - ${player.team.abbreviation}`;
+        option.addEventListener("click", () => {
+          dropdown.classList.remove("is-active");
+          input.value = player.first_name + " " + player.last_name;
+        });
+
+        resultsWrapper.appendChild(option);
+      }
+    }
+  }
+  if (players.length === 0) {
+    dropdown.classList.remove("is-active");
+  }
+};
+input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
